06167c3c7c55f2e9a20ce59d0b051c0a0113c817
zabachok <zabachok@gmail.com>
2023-02-27 18:14:58 +0300

    Moved files from symfobooster.

---
+++ src/Maker/Endpoint/Manifest/Input.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zabachok\Symfobooster\Maker\Endpoint\Manifest;
+
+use Zabachok\Symfobooster\Hydrator;
+
+class Input
+{
+    /** @var Field[] */
+    public array $fields = [];
+    public bool $hasMuted = false;
+    public bool $hadRenamed = false;
+
+    public function setFields(array $fields): void
+    {
+        $hydrator = new Hydrator();
+        foreach ($fields as $key => $manifest) {
+            /** @var Field $field */
+            $field = $hydrator->hydrate(Field::class, $manifest  ?? ['name' => $key]);
+            $field->name = $key;
+            if ($field->muted) {
+                $this->hasMuted = true;
+            }
+            if ($field->renamed) {
+                $this->hadRenamed = true;
+            }
+            $this->fields[] = $field;
+        }
+    }
+}
---
+++ src/Maker/Endpoint/Manifest/Service.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zabachok\Symfobooster\Maker\Endpoint\Manifest;
+
+class Service
+{
+    public bool $transactional;
+    public string $repository;
+    public string $entity;
+}
