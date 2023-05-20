3fa6f07601db823ba87a486248e02a08e1b1363e
zabachok <zabachok@gmail.com>
2023-05-20 19:40:12 +0300

    Installation fixes.

--- SymfoboosterDevkitBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zabachok\SymfoboosterDevkit;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfoboosterDevkitBundle extends Bundle
-{
-}
---
+++ src/SymfoboosterDevkitBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zabachok\SymfoboosterDevkit;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SymfoboosterDevkitBundle extends AbstractBundle
+{
+}
