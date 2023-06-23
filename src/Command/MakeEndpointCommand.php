554a7a56023865d1ff35e08ae1f4ceedb6ee64f7
zabachok <zabachok@gmail.com>
2023-06-23 19:34:36 +0300

    Starts to make new command for generators

---
+++ src/Command/MakeEndpointCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Symfobooster\Devkit\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'symfobooster:make:endpoint',
+    description: 'Make new endpoint',
+    hidden: false,
+)]
+class MakeEndpointCommand extends Command
+{
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
